57655c9ce0da74aebbb3c21ce2a4261e66c5e0e8
mrygiel <mrygiel1990@gmail.com>
2022-11-05 12:34:11 +0100

    update

---
+++ src/FamilyMember.java
@@ -0,0 +1,6 @@
+public class FamilyMember {
+    static String surname = "Nowak"; // zmienna jest dla calej klasy a nie dla poszczegolnego obiektu
+    String name;
+    int age;
+
+}
---
+++ src/FamilyMemberTester.java
@@ -0,0 +1,13 @@
+public class FamilyMemberTester {
+    public static void main(String[] args) {
+        FamilyMember dad = new FamilyMember();
+        dad.name = "Karol";
+        dad.age = 34;
+        System.out.println("Nazwisko rodu to: " + FamilyMember.surname); //ten zapis jest preferowany bo klasa ma zmienna statyczna
+//        System.out.println("Nazwisko rodu to: " + dad.surname);
+
+        FamilyMember mother = new FamilyMember();
+        mother.name = "Dorota";
+        mother.age = 30;
+    }
+}
