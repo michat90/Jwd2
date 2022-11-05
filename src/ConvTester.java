b1343440ad4f77ac9db70b9041614a036dff3c91
mrygiel <mrygiel1990@gmail.com>
2022-11-05 13:07:03 +0100

    update

---
+++ src/ConvTester.java
@@ -0,0 +1,23 @@
+public class ConvTester {
+    public static void main(String[] args) {
+
+//        short firstPrice = 234;
+//        short secondPrice = 178;
+//
+//        System.out.println(Math.max(firstPrice,secondPrice)); // przy Integer nastepuje auto uboxing i funcja tez dziala
+//
+//        byte a = 7;
+//        short b;
+//        int c;
+//
+//        c = a; // konwersja niejawna gdy do wiekszego pudelka pakuje mniejsze, odwrotnie blad
+//        b = (short) c; // jawna konwersja - rzutowanie
+
+        int a = 5;
+        double b = 13.5;
+        //double c = b / a;
+        int c = ((int)b) / a;
+
+
+    }
+}
