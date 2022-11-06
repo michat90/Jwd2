396bc579ecac3a8c072818d0a3033b101d3da72c
mrygiel <mrygiel1990@gmail.com>
2022-11-06 15:48:43 +0100

    update

--- src/FrequentNumber.java
+++ src/FrequentNumber.java
@@ -1,8 +1,9 @@
+//Napisz program, który znajdzie najczęściej występującą cyfrę w tablicy.
 public class FrequentNumber {
     public static void main(String[] args) {
 
-        int[] arr = {3, 5, 3, 1, 6, 8, 1, 2, 4, 3, 6, 7,5,2,4,2,6,7,8,6,9,1,3,2,4,3,8,4,4,4,0};
-        int [] counters = new int[10];
+        int[] arr = {3, 5, 3, 1, 6, 8, 1, 2, 4, 3, 6, 7, 5, 2, 4, 2, 6, 7, 8, 6, 9, 1, 3, 2, 4, 3, 8, 4, 4, 4, 0};
+        int[] counters = new int[10];
 
         for (int i = 0; i < arr.length; i++) {
             counters[arr[i]]++;            // zwiększa o 1 kazdy element tablicy gdy napotkamy liczbe
@@ -13,7 +14,7 @@ public class FrequentNumber {
 
         int mostFrequent = 0;
 
-        for (int i = 0; i <  counters.length; i++) {
+        for (int i = 0; i < counters.length; i++) {
             if (counters[i] > counters[mostFrequent]) {
                 mostFrequent = i;
             }
