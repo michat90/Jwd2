cddb65fbccd03d09692a7cb0cda3ca18a24850aa
mrygiel <mrygiel1990@gmail.com>
2023-01-07 16:09:44 +0100

    update

---
+++ src/zajecia3/Product.java
@@ -0,0 +1,23 @@
+package zajecia3;
+
+import java.util.Objects;
+
+public class Product {
+    public String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 && Objects.equals(name, product.name);
+    }
+
+
+}
